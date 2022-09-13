d860c24d430a8eccbc647fe5119e376d6e9951e3
Diaa-E <blarb21@yahoo.com>
2022-09-13 03:58:58 +0200

    Add the flashcards  data object

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 "use strict"
 
+//key and pair names are changed based on purpose
+//Sould be fed from the JSON in the future
+const key = "du";
+const value = "en";
+
+//Data pairs object
+const data = [
+    {
+        [key]: "Apfel",
+        [value]: "Apple"
+    },
+    {
+        [key]: "Haus",
+        [value]: "House"
+    }
+];
