948f42837bdbdb853ebeafff8d9a7b4b5d29d494
Diaa-E <blarb21@yahoo.com>
2022-09-13 04:20:37 +0200

    Add 'next' button toggling

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-"use strict"
+"use strict";
+
+//Global vars------------------------------
 
 //key and pair names are changed based on purpose
 //Sould be fed from the JSON in the future
 const key = "du";
 const value = "en";
+let isSubmitted = false;
 
 //Data pairs object
 const data = [
@@ -16,3 +19,43 @@ const data = [
         [value]: "House"
     }
 ];
+
+
+//document elements---------------------------
+const btnSubmit = document.querySelector("#submit");
+const btnNext = document.querySelector("#next");
+
+//Event listeners-----------------------
+btnSubmit.addEventListener("click", () =>{
+    submitAns();
+});
+
+btnNext.addEventListener("click", () =>{
+    getNextCard();
+});
+
+//Functions-------------------------------
+
+function submitAns()
+{
+    isSubmitted = true;
+    toggleNext();
+};
+
+function getNextCard()
+{
+    isSubmitted = false;
+    toggleNext();
+};
+
+function toggleNext()
+{
+    if (isSubmitted)
+    {
+        btnNext.disabled = false;
+    }
+    else
+    {
+        btnNext.disabled = true;
+    }
+};
